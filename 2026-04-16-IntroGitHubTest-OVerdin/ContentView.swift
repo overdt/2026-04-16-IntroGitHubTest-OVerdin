4654747c0d0ea678646b40e2e4863518b03f61e3
Omar Verdin <omar.verdin31@gmail.com>
2026-04-16 16:30:44 -0700

    Initial Commit

---
+++ 2026-04-16-IntroGitHubTest-OVerdin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2026-04-16-IntroGitHubTest-OVerdin
+//
+//  Created by Omar Verdin on 4/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2026-04-16-IntroGitHubTest-OVerdin/_026_04_16_IntroGitHubTest_OVerdinApp.swift
@@ -0,0 +1,17 @@
+//
+//  _026_04_16_IntroGitHubTest_OVerdinApp.swift
+//  2026-04-16-IntroGitHubTest-OVerdin
+//
+//  Created by Omar Verdin on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct _026_04_16_IntroGitHubTest_OVerdinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

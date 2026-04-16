705b16bb9b21647ba41023a9e334c3880450d1f1
Omar Verdin <omar.verdin31@gmail.com>
2026-04-16 16:42:32 -0700

    changed the image to resizable and scaled it to fit

--- 2026-04-16-IntroGitHubTest-OVerdin/ContentView.swift
+++ 2026-04-16-IntroGitHubTest-OVerdin/ContentView.swift
@@ -11,7 +11,8 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
-                .imageScale(.large)
+                .resizable()
+                .scaledToFit()
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }
